6a6291e2948e1a574858b3ea0b223034d2d211b7
Andrew Grathwohl <andrew@sacreddata.io>
2015-11-24 15:13:36 -0500

    added OPUS audio so that vp9 creation is canonical

--- ppt.py
+++ ppt.py
@@ -36,7 +36,8 @@ class PPT:
         analyze = [FFPROBE_BIN, '-v', 'quiet', '-show_format', '-show_streams',
                    '-print_format', 'json', self.filename]
         probe_data = sp.check_output(analyze, bufsize=10**8, stderr=sp.STDOUT)
-        self.probe_res = json.loads(probe_data.decode(encoding='UTF-8'), 'latin-1')
+        self.probe_res = json.loads(
+            probe_data.decode(encoding='UTF-8'), 'latin-1')
         self.analysis = json.dumps(self.probe_res, indent=4)
         stm_data = self.probe_res["streams"]
         for stream in stm_data:
@@ -61,6 +62,7 @@ class PPT:
         else:
             new_mbps = round(new_mbps)
             # Determine minimum+maximum bit rate + buffer size
+            # When VP10 is released, these vals will matter!
             min_rate = round(new_mbps * 0.85)
             max_rate = round(new_mbps * 1.5)
             buf_size = round(max_rate * 1.75)
@@ -78,7 +80,7 @@ class PPT:
         sp.check_output(y4m_cmd, stderr=sp.STDOUT)
         print("Y4M creation complete.")
 
-    def build_vorbis(self):
+    def build_audio(self):
         print("Stream copying source audio to WAV for OGG conversion.")
         self.wav_in = self.filename + '.wav'
         wav_cmd = [FFMPEG_BIN, '-y', '-i', self.filename, '-vn', '-sn', '-map',
@@ -87,9 +89,13 @@ class PPT:
         print("Converting source audio into OGG Vorbis for WebM stream copy.")
         self.ogg_out = self.filename + '.ogg'
         ogg_start = ['oggenc', self.wav_in, '-o', self.ogg_out]
-        print(ogg_start)
         sp.check_output(ogg_start, stderr=sp.STDOUT)
         print("OGG Vorbis conversion complete.")
+        print("Encoding WAV audio into OPUS for WebM stream copy.")
+        self.opus_out = self.filename + '.opus'
+        opus_cmd = ['opusenc', '--bitrate', '160', self.wav_in, self.opus_out]
+        sp.check_output(opus_cmd, stderr=sp.STDOUT)
+        print("OPUS encoding complete.")
 
     def build_vp8(self):
         print("Converting source video to VP8 stream.")
@@ -107,7 +113,7 @@ class PPT:
         print("Converting source video to VP9 stream.")
         y4m_in = self.filename + '.y4m'
         self.vp9_out = self.filename + '.vp9.webm'
-        vp9_target = int(self.target / 2)
+        vp9_target = int(self.target / 1.5)
         vp9_opts = self.opts['formats']['vp9']['vpxenc'].split(' ')
         vp9_bits = '--target-bitrate=' + str(vp9_target)
         vp9_args = [vp9_bits, '-o', self.vp9_out, y4m_in]
@@ -118,14 +124,25 @@ class PPT:
 
     def build_webm(self):
         self.webm_ref = self.filename + '.ref.webm'
+        self.vp8_ref = '.vp8' + self.webm_ref
+        self.vp9_ref = '.vp9' + self.webm_ref
         # Put it all together
-        ffmpeg_copy = [FFMPEG_BIN, '-y', '-i', self.vp8_out, '-i', self.ogg_out,
-                       '-c', 'copy', '-flags', '+global_header', self.webm_ref]
-        sp.check_output(ffmpeg_copy, stderr=sp.STDOUT)
+        ffmpeg_copy_vp8 = [FFMPEG_BIN, '-y', '-i', self.vp8_out, '-i',
+                           self.ogg_out, '-c', 'copy', '-flags',
+                           '+global_header', self.vp8_ref]
+        sp.check_output(ffmpeg_copy_vp8, stderr=sp.STDOUT)
+        ffmpeg_copy_vp9 = [FFMPEG_BIN, '-y', '-i', self.vp9_out, '-i',
+                           self.opus_out, '-c', 'copy', '-flags',
+                           '+global_header', self.vp9_ref]
+        sp.check_output(ffmpeg_copy_vp9, stderr=sp.STDOUT)
 
     def multi_webm(self):
         # Now that the primary encodes are done, we can make the compatability
         # encodes. These are to be created from the initial 2-pass encode.
+        # Note: this method outputs a string to a shell script for execution.
+        # This is done in order to allow the server admin to designate niceness
+        # on a per-job basis. Regardless, Python's subprocess module cannot
+        # reconcile the canonical FFmpeg syntax required to run this command.
         if self.video_stream["coded_height"] >= 1080:
             mult_begin = 'webm_1080'
         elif self.video_stream["coded_height"] >= 720:
@@ -158,18 +175,17 @@ class PPT:
             map_num += 1
         mult_start = [FFMPEG_BIN, '-y', '-i', self.webm_ref]
         mult_command = ['-filter_complex'] + mult_complex + mult_map
+        mult_sh = mult_start + mult_command
         print("Multiple-bitrate compatability encodes:")
-        print(mult_command)
+        print(mult_sh)
         cc_output = ""
-        for c in mult_command:
+        for c in mult_sh:
             cc_output += c
             cc_output += " "
-        # print("Insert the following command string into your FFmpeg wrapper: ")
-        # print(cc_output)
-        sh_output = 'ffmpeg -y -i ' + str(self.webm_ref) + ' ' + cc_output
-        f = open("ffmpeg.sh","w")
-        f.write(sh_output)
+        f = open("ffmpeg.sh", "w")
+        f.write(cc_output)
         f.close()
+        sp.check_output(['sh', 'ffmpeg.sh'], stderr=sp.STDOUT)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -177,9 +193,10 @@ if __name__ == '__main__':
     args = parser.parse_args()
     print(args.file)
     p = PPT()
-    p.analyze(args.file)
-    p.build_y4m()
-    p.build_vorbis()
-    p.build_vp8()
-    p.build_webm()
-    p.multi_webm()
+    p.analyze(args.file)  # Grab the file, probe for validity, analyze metadata
+    p.build_y4m()  # Create the source file for use on x264 and VP8 ref videos
+    p.build_audio()  # Grab the file's audio stream, output OGG+OPUS
+    p.build_vp8()  # Create the VP8 stream for insertion into WebM container
+    p.build_vp9()  # Create the VP9 stream for insertion into WebM container
+    p.build_webm()  # Build the reference WebM file, from which all others stem
+    p.multi_webm()  # Create multi-output FFmpeg script for all other WebMs
